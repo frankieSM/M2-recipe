bba6802cf4b83be539343a8ee74070f68233d708
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-09-05 13:37:44 -0700

    Attempting to debug react alice carousel, need help

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 function Popular() {
     const [popular, setPopular] = useState([]);
@@ -17,35 +19,60 @@ function Popular() {
         setPopular(data.recipes)
     }
 
+    const carouselOptions = {
+        items: 3,
+        autoPlay: true,
+        // Auto-play interval
+        autoPlayInterval: 3000, 
+        stopAutoPlayOnHover: true,
+        mouseTracking: true,
+        responsive: {
+            // Smaller screens
+            0: { items: 1 },
+            // Medium screens
+            768: { items: 3 }, 
+        },
+    };
+
     return (
         <div>
-            <h2>Popular Recipes</h2>
-            {popular.map(recipe => {
-                return (
-                  <Carousel>
-                    <h2>Popular Recipes</h2>
-                    {popular.map((recipe) => {
-                        return(
-                            <Card>
+            <CarouselContainer>
+                <h2>Popular Recipes</h2>
+                {popular.length > 0 ? (
+                    <AliceCarousel responsive={carouselOptions}>
+                        {popular.map((recipe) => (
+                            <Card key={recipe.id}>
                                 <p>{recipe.title}</p>
                                 <img src={recipe.image} alt={recipe.title} />
                             </Card>
-                        );
-                    })}
-                  </Carousel>  
-                )
-            }
-            )}
+                        ))}
+                    </AliceCarousel>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </CarouselContainer>
         </div>
     )
 }
 
-const Carousel = styled.div`
-    margin: 4rem 0rem;
-    `;
-
 const Card = styled.div`
-    min-height: 25rem;
-`
+    min-height: 18rem;
+    width: 33.33%;
+    margin: 0 1rem;
+    
+    img {
+        width: 100%;
+        border-radius: 1rem;
+    }
+`;
+
+const CarouselContainer = styled.div`
+    width: 100%;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+        width: 80%; /* Adjust the width for larger screens */
+    }
+`;
 
 export default Popular;
