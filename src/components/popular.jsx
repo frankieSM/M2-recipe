51fa458b5abfa23048e52617c78c8ea247c36699
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-09-05 13:39:27 -0700

    Scrapping Alice carousel

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
 
 function Popular() {
     const [popular, setPopular] = useState([]);
@@ -19,60 +17,35 @@ function Popular() {
         setPopular(data.recipes)
     }
 
-    const carouselOptions = {
-        items: 3,
-        autoPlay: true,
-        // Auto-play interval
-        autoPlayInterval: 3000, 
-        stopAutoPlayOnHover: true,
-        mouseTracking: true,
-        responsive: {
-            // Smaller screens
-            0: { items: 1 },
-            // Medium screens
-            768: { items: 3 }, 
-        },
-    };
-
     return (
         <div>
-            <CarouselContainer>
-                <h2>Popular Recipes</h2>
-                {popular.length > 0 ? (
-                    <AliceCarousel responsive={carouselOptions}>
-                        {popular.map((recipe) => (
-                            <Card key={recipe.id}>
+            <h2>Popular Recipes</h2>
+            {popular.map(recipe => {
+                return (
+                  <Carousel>
+                    <h2>Popular Recipes</h2>
+                    {popular.map((recipe) => {
+                        return(
+                            <Card>
                                 <p>{recipe.title}</p>
                                 <img src={recipe.image} alt={recipe.title} />
                             </Card>
-                        ))}
-                    </AliceCarousel>
-                ) : (
-                    <p>Loading...</p>
-                )}
-            </CarouselContainer>
+                        );
+                    })}
+                  </Carousel>  
+                )
+            }
+            )}
         </div>
     )
 }
 
+const Carousel = styled.div`
+    margin: 4rem 0rem;
+    `;
+
 const Card = styled.div`
-    min-height: 18rem;
-    width: 33.33%;
-    margin: 0 1rem;
-    
-    img {
-        width: 100%;
-        border-radius: 1rem;
-    }
-`;
-
-const CarouselContainer = styled.div`
-    width: 100%;
-    margin: 0 auto;
-
-    @media (min-width: 768px) {
-        width: 80%; /* Adjust the width for larger screens */
-    }
-`;
+    min-height: 25rem;
+`
 
 export default Popular;
