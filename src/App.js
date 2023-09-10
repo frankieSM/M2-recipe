bb24780913a464669e5d83a3dadd38b8560fc7e3
Leeann Saengsavang <125524497+lsaengsavang@users.noreply.github.com>
2023-09-09 17:00:27 -0700

    Added more styling to the home page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
-// import { LuUtensils } from "react-icons/lu";
-// import styled from "styled-components";
-// import { Link } from "react-router-dom"
+import ironDevsLogo from "./images/iron-devs-logo.png"
+//import { LuUtensils } from "react-icons/lu";
+import styled from "styled-components";
+//import { Link } from "react-router-dom"
 
 
 function App() {
   return (
     <div className="App">
+      <PageTitle>Hell's Kitchen</PageTitle>
+      <LogoImage src={ironDevsLogo} alt="Iron Devs Logo" />
       <BrowserRouter>
         <Search />
         <Category />
@@ -19,9 +22,20 @@ function App() {
   );
 }
 
-/* const Logo = styled(Link)`
-  text-decoration: none;
-  font-family: 'Bebas Neue', sans-serif;
-` */
+
+const LogoImage = styled.img`
+  width: 150px;
+  height: auto;
+  border: 2px solid #FFFFFF;
+  border-radius: 5px;
+  margin-left: 20px;
+`;
+
+const PageTitle = styled.h1`
+  text-align: center;
+  margin-top: 30px;
+  color: #FFFFFF;
+  font-size: 2rem;
+`;
 
 export default App;
